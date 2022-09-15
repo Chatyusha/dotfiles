96d0766d1f08eec3ae9608ee891e54d138e8ea2b
Chatyusha <hirounicat76@gmail.com>
2022-09-15 19:26:25 +0900

    update

--- zsh/git_prompt.zsh
+++ zsh/git_prompt.zsh
@@ -3,12 +3,20 @@
 autoload -U colors; colors
 
 function git-prompt {
-  local current_branch="$(git symbolic-ref --short HEAD 2> /dev/null)"
   if [ ! -e  ".git" ]; then
      # git 管理されていないディレクトリは何も返さない
      return
   fi
-  local show_current_branch="${fg[green]}[${current_branch}]${reset_color}"
+  local current_branch rep_status show_current_branch
+  current_branch="$(git symbolic-ref --short HEAD 2> /dev/null)"
+
+  rep_status=$(git status 2> /dev/null)
+
+  if [[ -n $(echo $rep_status | grep "^nothing to") ]];then
+    show_current_branch="${fg[cyan]}[${current_branch}]${reset_color}"
+  elif [[ -n $(echo $rep_status | grep "^no changes added to commit") ]]; then 
+    show_current_branch="${fg[red]}[${current_branch}]${reset_color}"
+  fi
   echo "${show_current_branch}"
 }
 
