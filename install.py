4af1f4ec2b60b1abf4f9c9a73d0e0531b0223f28
Chatyusha <hirounicat76@gmail.com>
2022-09-28 13:58:44 +0000

    Upd

---
+++ install.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+def ClearLine(col: int):
+
+
+
+def ShowMenu():
+    print("---- MENU ----")
+
+
+def main():
+    print("hoge")
+    print("fuga", end="")
+    ClearLine(2)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
---
+++ vim/vim/ddcconf.vim
@@ -0,0 +1,7 @@
+" Customize global settings
+" Use around source.
+" https://github.com/Shougo/ddc-around
+call ddc#custom#patch_global('sources', ['around'])
+
+" Use ddc.
+call ddc#enable()
